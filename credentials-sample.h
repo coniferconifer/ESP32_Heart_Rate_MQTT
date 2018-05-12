f4d90f91bf45990a3c721a78163a836e02b48b94
coniferconifer <doikatsuo@yahoo.co.jp>
2018-05-12 19:01:49 +0900

    initial version

---
+++ credentials-sample.h
@@ -0,0 +1,16 @@
+#define WIFI_SSID "aterm-3933ce-h"
+#define WIFI_PASS  "xxxxxxxxx"
+
+#define WIFI_SSID1 "Planex_24"
+#define WIFI_PASS1  "yyyyyyyyyy"
+
+#define WIFI_SSID2 "APatworkplace"
+#define WIFI_PASS2  "zzzzzzzzzz"
+
+#define TOKEN "AAAAAAAAAAAAA"
+#define TOKEN1 "AAAAAAAAAAAAA"
+#define TOKEN2 "BBBBBBBBBBBBBB"
+
+#define SERVER "192.168.13.201"
+#define SERVER1 "192.168.13.201"
+#define SERVER2 "192.168.52.24"
